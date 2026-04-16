6213b5e399d07a19bf726a1322a8ce27d2c9c571
winno <rafiwinno13@gmail.com>
2026-04-16 08:16:29 +0700

    memperbaiki sidebar dan icon

--- backend/app/Http/Controllers/Api/User/KursusController.php
+++ backend/app/Http/Controllers/Api/User/KursusController.php
@@ -81,7 +81,7 @@ class KursusController extends Controller
 
         DB::table('progress_materi')->updateOrInsert(
             ['id_pengguna' => $id_pengguna, 'id_materi' => $id_materi],
-            ['status' => 'selesai', 'updated_at' => now()]
+            ['status' => 'selesai', 'waktu_update' => now()]
         );
 
         return response()->json(['message' => 'Progress disimpan']);
